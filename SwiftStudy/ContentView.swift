1d69c42b7ed8768e826106030b1552abc3e992d3
dima <dima@kojem.ru>
2023-10-30 16:36:27 +0300

    Initial Commit

---
+++ SwiftStudy/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftStudy
+//
+//  Created by Дима Кожемякин on 30.10.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftStudy/SwiftStudyApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftStudyApp.swift
+//  SwiftStudy
+//
+//  Created by Дима Кожемякин on 30.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftStudyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
