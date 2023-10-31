e829b42aa2c76755c280715767aa6b1bfc2b5e72
dima <dima@kojem.ru>
2023-10-31 12:28:07 +0300

    Parth 1

---
+++ SwiftStudy/FirstQuestionsView.swift
@@ -0,0 +1,18 @@
+//
+//  FirstQuestionsView.swift
+//  SwiftStudy
+//
+//  Created by Дима Кожемякин on 30.10.2023.
+//
+
+import SwiftUI
+
+struct FirstQuestionsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FirstQuestionsView()
+}
---
+++ SwiftStudy/Funces.swift
@@ -0,0 +1,8 @@
+//
+//  Funces.swift
+//  SwiftStudy
+//
+//  Created by Дима Кожемякин on 30.10.2023.
+//
+
+import Foundation
